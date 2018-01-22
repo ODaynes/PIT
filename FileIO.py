8b38bdbaa89feb6ec94438eddd07106b4c6f17ee
odayne <odayne@essex.ac.uk>
2018-01-22 13:04:41 +0000

    New library (docx2txt) has been imported which may make text extraction from doc/docx files easier than before. Implementation of read_docx has been created with aforementioned library.
    get_extension() method has been added

--- FileIO.py
+++ FileIO.py
@@ -6,13 +6,38 @@ Designed and created by Owen Daynes.
 
 DOCX is a library designed to create and update Microsoft Word files.
 More information can be found here: https://python-docx.readthedocs.io/en/latest/
+
+docx2txt is a library designed to extract text from doc and docx files.
+More information can be found here: https://github.com/ankushshah89/python-docx2txt
 """
 
+import os
+
 import docx
+import docx2txt
+
+
+def get_extension(filepath):
+    return os.path.splitext(filepath)[1][1:].lower()
+
+
+""" 
+Generic method to get text from file
+Calls read_txt, read_doc and read_docx
+dependant on file extension.
+"""
 
 
 def read_file(filepath):
-    return
+
+    ext = get_extension(filepath)
+
+    if ext == "txt":
+        return read_txt(filepath)
+    if ext == "doc" or ext == "docx":
+        return read_docx(filepath)
+
+    return None
 
 
 def read_txt(filepath):
@@ -24,7 +49,7 @@ def read_doc(filepath):
 
 
 def read_docx(filepath):
-    return
+    return docx2txt.process(filepath)
 
 
 def normalise(document):
