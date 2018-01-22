9cab066627410453b4fa9c91dc19cba008a342f8
odayne <odayne@essex.ac.uk>
2018-01-22 12:15:51 +0000

    Created empty function definitions for file reading and normalisation, added commented signature

--- FileIO.py
+++ FileIO.py
@@ -1,1 +1,43 @@
-print("Hello, world!")
+"""
+FileIO.py should include all methods used to read
+and normalise files before both comparison.
+
+Designed and created by Owen Daynes.
+
+DOCX is a library designed to create and update Microsoft Word files.
+More information can be found here: https://python-docx.readthedocs.io/en/latest/
+"""
+
+import docx
+
+
+def read_file(filepath):
+    return
+
+
+def read_txt(filepath):
+    return
+
+
+def read_doc(filepath):
+    return
+
+
+def read_docx(filepath):
+    return
+
+
+def normalise(document):
+    return
+
+
+def normalise_doc(document):
+    return
+
+
+def normalise_docx(document):
+    return
+
+
+def normalise_txt(document):
+    return
