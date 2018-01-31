7d72aa2d1867589e6c29f322a64721baf54359f9
Owen Daynes <OwenDaynes@hotmail.co.uk>
2018-01-31 18:11:41 +0000

    Created TextProcessing.py to contain all methods regarding text processing and manipulation.
    Added templates for punctuation removal and bag of words functions

---
+++ TextProcessing.py
@@ -0,0 +1,28 @@
+"""
+TextProcessing.py should include all methods used to process
+and otherwise manipulate text.
+
+Designed and created by Owen Daynes.
+
+NLTK (natural language tool kit) is a library with a variety of
+different natural language processing utilities.
+More information can be found here: http://www.nltk.org/
+"""
+
+"""
+Takes a string representation of a document as the parameter
+Returns a string representation of a document with all punctuation removed
+"""
+
+
+def remove_punctuation(document):
+    return
+
+"""
+Takes a string representation of a document as the parameter
+Returns a list containing each unique word in the provided document 
+"""
+
+
+def word_bag(document):
+    return
