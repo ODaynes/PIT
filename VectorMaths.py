5ec8a3b7fe5dc1438d163a0cdf7631910b627d2a
Owen Daynes <OwenDaynes@hotmail.co.uk>
2018-03-03 15:46:15 +0000

    created and implemented calculate_norm function
    created and implemented cosine_similarity function

--- VectorMaths.py
+++ VectorMaths.py
@@ -6,6 +6,8 @@ Designed and created by Owen Daynes.
 
 """
 
+import math
+
 """
 Takes two vectors
 Returns dot product of vectors
@@ -17,3 +19,32 @@ def dot_product(document1, document2):
     for i in range(0, len(document1)):
         result += document1[i] * document2[i]
     return result
+
+
+"""
+Takes a vector as a parameter
+Returns the euclidean norm of the vector
+"""
+
+
+def calculate_norm(vector):
+
+    norm = 0
+    for element in vector:
+        norm += element**2
+    return math.sqrt(norm)
+
+
+"""
+Takes two document vectors
+Returns cosine similarity between vectors
+"""
+
+
+def cosine_similarity(document1, document2):
+    dot = dot_product(document1, document2)
+
+    d1 = calculate_norm(document1)
+    d2 = calculate_norm(document2)
+
+    return dot * (d1 / d2)
