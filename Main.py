3e564176393b2f984ec40ae7b66c6fb42a5073dd
Owen Daynes <OwenDaynes@hotmail.co.uk>
2018-03-06 01:03:20 +0000

    implemented removal of stopwords
    added inverse document frequencies to container object
    
    created variable for normalised_documents list to allow for reuse
    
    created and implemented method to create a dense vector, needs to be moved to appropriate py file once finished

--- Main.py
+++ Main.py
@@ -3,10 +3,10 @@ import os
 from FileIO import *
 from FileContainer import Container
 from TextProcessing import *
+from VectorMaths import *
+
 from nltk.corpus import stopwords
 from string import punctuation
-from copy import copy
-from math import log
 
 
 def main():
@@ -46,7 +46,7 @@ def main():
 
             c.raw = file_contents
             c.tokens = word_tokenize(c.raw)
-            c.normalised = [token.lower() for token in c.tokens if token not in punctuation] # token.lower() not in stopwords.words("english") and
+            c.normalised = [token.lower() for token in c.tokens if token not in punctuation and token.lower() not in stopwords.words("english")] # token.lower() not in stopwords.words("english") and
             [vocabulary.append(token) for token in c.normalised if token not in vocabulary]
 
             for word in c.normalised:
@@ -57,14 +57,23 @@ def main():
                     global_word_counts[word] = global_word_counts[word] + 1
 
             c.term_frequencies = dict()
+            c.inverse_document_frequencies = dict()
 
             containers.append(c)
 
-    # calculate term frequencies for each term in each file
+    # collect normalised documents in a single list to be used in idf calculation
+
+    normalised_documents = [container.normalised for container in containers]
+
+    # calculate term frequencies and inverse document frequencies for each term in each file
 
     for container in containers:
         for word in vocabulary:
-            container.term_frequencies[word] = container.normalised.count(word) / len(container.normalised)
+            if container.normalised.count(word) > 0:
+                container.term_frequencies[word] = container.normalised.count(word) / len(container.normalised)
+            container.inverse_document_frequencies[word] = calculate_idf(word, normalised_documents)
+        container.vector = create_document_vector(container)
+
 
     default_frequency_map = dict()
 
@@ -73,23 +82,29 @@ def main():
 
     # debugging test
 
-    document = containers[0]
+    x, y = create_dense_vectors(containers[1], containers[2])
+    z = cosine_similarity(x, y)
 
-    print(document.term_frequencies["learning"] * calculate_idf("learning", [c.normalised for c in containers]))
+    print(z)
 
-
-"""
-Takes two TD-IDF vectors
-Returns dot product of vectors
-"""
-
-
-def dot_product(document1, document2):
-    result = 0
-    for i in range(0, len(document1)):
-        result += document1[i] * document2[i]
-    return result
+    # print(vocabulary)
+    # print(cosine_similarity(containers[0].vector, containers[1].vector))
+    # print(cosine_similarity(containers[0].vector, containers[2].vector))
+    # print(cosine_similarity(containers[0].vector, containers[3].vector))
+    # print(cosine_similarity(containers[1].vector, containers[2].vector))
 
 
+def create_dense_vectors(container_x, container_y):
+    shared_vocabulary = list(set(container_x.normalised).intersection(set(container_y.normalised)))
+    # shared_vocabulary = set([term for term, frequency in container_x.term_frequencies.items() if frequency > 0]).intersection(set([term for term, frequency in container_y.term_frequencies.items() if frequency > 0]))
+    new_x, new_y = list(), list()
+
+    for term in shared_vocabulary:
+        new_x.append(container_x.term_frequencies[term] * container_x.inverse_document_frequencies[term])
+        new_y.append(container_y.term_frequencies[term] * container_y.inverse_document_frequencies[term])
+
+    return new_x, new_y
+
+#def new_cosine_similarity(vector_x, vector_y, )
 if __name__ == "__main__":
     main()
