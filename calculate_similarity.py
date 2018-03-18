6f52fbbf405148e1bec74decac56c31877c9291a
Owen Daynes <OwenDaynes@hotmail.co.uk>
2018-03-18 23:41:04 +0000

    stylised window

--- calculate_similarity.py
+++ calculate_similarity.py
@@ -1,10 +1,12 @@
 from tkinter import messagebox
+from tkinter import filedialog
 
 from Main import main
 
 from tkinter import *
-import threading
 
+
+# opens pop up with information about application
 
 def launch_about_window():
     msg = "Plagiarism Indication Tool (PIT)" \
@@ -15,9 +17,33 @@ def launch_about_window():
     messagebox.showinfo("About", msg)
 
 
+# opens pop up with error messages
+
+def error_popup(error_list):
+
+    msg = ""
+
+    for error in error_list:
+        msg += error + "\n\n"
+
+    messagebox.showerror("Error encountered", msg.strip())
+
+
+# kills application
+
 def exit_prog():
     sys.exit(0)
 
+
+# launches file explorer, enters selected directory into appropriate field
+
+def get_directory():
+    directory = filedialog.askdirectory()
+    directory_entry.delete(0, END)
+    directory_entry.insert(0, directory)
+
+
+# checks inputs to see if all inputs are valid, returns errors in list
 
 def check_inputs(directory, threshold):
     error_list = list()
@@ -36,63 +62,70 @@ def check_inputs(directory, threshold):
 
     return error_list
 
+
+# gathers required data and calls main method to calculate similarity
+
 def calculate():
-    errors_encountered = False
 
     directory = directory_entry.get()
     threshold = threshold_entry.get()
+    include = included_check.get()
 
     error_list = check_inputs(directory, threshold)
 
     if len(error_list) > 0:
         error_popup(error_list)
     else:
-        threading.Thread(target=main(directory_entry.get(), threshold_entry.get())).start()
+        main(directory, threshold, include)
+        messagebox.showinfo("Success", "Report generated!")
 
-def error_popup(error_list):
-
-    msg = ""
-
-    for error in error_list:
-        msg += error + "\n\n"
-
-    messagebox.showerror("Error encountered", msg.strip())
 
 root = Tk()
-root.geometry("600x400")
+root.configure(background="#a1dbcd")
+root.geometry("550x200")
 root.title("Plagiarism Indicition Tool (PIT)")
 
+title_label = Label(root, text="Plagiarism Indication Tool", background="#a1dbcd", font=("Helvetica", 18))
+title_label.grid(row=0, column=3, columnspan=1)
 
-directory_label = Label(root, text="Directory")
-directory_label.grid(row=0, column=0, columnspan=1)
+directory_label = Label(root, text="Directory", background="#a1dbcd")
+directory_label.grid(row=1, column=1, columnspan=1)
 
 directory_entry = Entry(root)
-directory_entry.grid(row=0, column=1, columnspan=1)
+directory_entry.grid(row=1, column=2, columnspan=2)
 
-threshold_label = Label(root, text="Threshold")
-threshold_label.grid(row=1, column=0, columnspan=1)
+directory_button = Button(root, text="Select directory", command=get_directory)
+directory_button.grid(row=1, column=4, columnspan=1)
+
+
+threshold_label = Label(root, text="Threshold", background="#a1dbcd")
+threshold_label.grid(row=2, column=1, columnspan=1)
 
 threshold_entry = Entry(root)
-threshold_entry.grid(row=1, column=1, columnspan=1)
+threshold_entry.grid(row=2, column=2, columnspan=2)
 
-# create a menu
+include_not_plagiarism_label = Label(root, text="Include inoffensive pairs", background="#a1dbcd")
+include_not_plagiarism_label.grid(row=3, column=1, columnspan=1)
+
+included_check = IntVar()
+include_not_plagiarism_entry = Checkbutton(root, variable=included_check, background="#a1dbcd")
+include_not_plagiarism_entry.grid(row=3, column=3, columnspan=1)
+
+process_button = Button(root, text="Calculate similarity", command=calculate)
+process_button.grid(row=4, column=3, columnspan=1)
+
 menu = Menu(root)
 root.config(menu=menu)
 
 filemenu = Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
 filemenu.add_command(label="Calculate similarity", command=calculate)
-# filemenu.add_command(label="Open...", command=calculate)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=exit_prog)
 
 helpmenu = Menu(menu)
 menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About...", command=launch_about_window)
-
-process_button = Button(root, text="Calculate similarity", command=calculate)
-process_button.grid(row=2, column=0, columnspan=1)
-
 
 if __name__ == "__main__":
     # path provided
