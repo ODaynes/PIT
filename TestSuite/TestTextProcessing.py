0168c195f3dc95dab14b8a51f613f90119303ac1
Owen Daynes <OwenDaynes@hotmail.co.uk>
2018-04-01 02:26:11 +0100

    added word bag and stemmer tests

--- TestSuite/TestTextProcessing.py
+++ TestSuite/TestTextProcessing.py
@@ -1,6 +1,8 @@
 import unittest
 import TextProcessing
 
+from nltk.stem import snowball
+
 
 class TestTextProcessing(unittest.TestCase):
 
@@ -10,3 +12,14 @@ class TestTextProcessing(unittest.TestCase):
         actual_processed = TextProcessing.remove_punctuation(raw)
 
         assert expected_processed == actual_processed
+
+    def test_word_bag(self):
+        before = "This is is an example text"
+        after = TextProcessing.word_bag(before)
+
+        assert len(after) == 5
+
+    def test_stem_document(self):
+        before = ["Computer", "Science"]
+        after = TextProcessing.stem_document(before, snowball.EnglishStemmer())
+        assert after[0] == "comput" and after[1] == "scienc"
