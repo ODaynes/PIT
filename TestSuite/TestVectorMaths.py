9c1e582cf0b231be3d3d96a475cf4675d4dcf539
Owen Daynes <OwenDaynes@hotmail.co.uk>
2018-04-01 02:51:26 +0100

    created test class for vector maths, need to implement tests

---
+++ TestSuite/TestVectorMaths.py
@@ -0,0 +1,11 @@
+import unittest
+import VectorMaths
+
+
+class TestVectorMaths(unittest.TestCase):
+
+    def test_dot_product(self):
+        assert True
+
+    def test_cosine_similarity(self):
+        assert True
