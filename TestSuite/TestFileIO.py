5b9e3ea24f7e349fd7cadb301eeb3c70ebad1731
Owen Daynes <OwenDaynes@hotmail.co.uk>
2018-04-23 00:01:49 +0100

    added example html file, added unit test for read_html method, named test names

--- TestSuite/TestFileIO.py
+++ TestSuite/TestFileIO.py
@@ -6,24 +6,28 @@ from os import remove
 
 class TestFileIO(unittest.TestCase):
 
-    def test_load_txt(self):
+    def test_read_txt(self):
         contents = FileIO.read_file("example_files/ExampleTXT.txt")
         assert contents is not False
 
-    def test_load_docx(self):
+    def test_read_docx(self):
         contents = FileIO.read_file("example_files/ExampleDOCX.docx")
         assert contents is not False
 
-    def test_load_pdf(self):
+    def test_read_pdf(self):
         contents = FileIO.read_file("example_files/ExamplePDF.pdf")
         assert contents is not False
 
-    def test_load_other_format(self):
+    def test_read_other_format(self):
         contents = FileIO.read_file("TestFileIO.py")
         assert contents is False
 
+    def test_read_html(self):
+        contents = FileIO.read_html("https://www.google.com")
+        assert contents is not False
+
     def test_generate_file_list(self):
-        NUMBER_OF_FILES = 3
+        NUMBER_OF_FILES = 4
         filelist = FileIO.generate_file_list("example_files", True)
         assert len(filelist) == NUMBER_OF_FILES
 
